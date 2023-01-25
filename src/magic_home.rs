fcd0ec307bbe89ac7c92227681cee571a9f20c33
João Coutinho <jafonsocoutinho@protonmail.com>
2023-01-25 11:32:42 +0000

    Fixed error

--- src/magic_home.rs
+++ src/magic_home.rs
@@ -156,8 +156,7 @@ impl MagicHomeAPI {
     pub fn set_rgb(&mut self, r: isize, g: isize, b: isize) -> Result<(), &'static str> {
         if !(0..=255).contains(&r) {
             Err("Invalid r value")
-        //} else if !(0..=255).contains(&g) {
-        } else if 0 <= g && g <= 255 {
+        } else if !(0..=255).contains(&g) {
             Err("Invalid g value")
         } else if !(0..=255).contains(&b) {
             Err("Invalid b value")
